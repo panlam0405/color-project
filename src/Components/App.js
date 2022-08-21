8489d050c1443b2c2fd90ea32d14e94c2dba5a4b
Panlam0405 <panlam0405@gmail.com>
2022-08-21 12:59:45 +0300

    added react-router-dom

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,20 @@ import { generatePalette } from "../colorHelpers";
 import { colors } from "../seedColors";
 import Palette from "./Palette";
 import "../styles/App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 	console.log(generatePalette(colors[5]));
 	return (
 		<div className='App'>
-			<Palette palette={generatePalette(colors[5])} />
+			<Routes>
+				<Route exact path='/' />
+				<Route
+					exact
+					path='/palette/:id'
+					element={<Palette palette={generatePalette(colors[5])} />}
+				/>
+			</Routes>
 		</div>
 	);
 }
