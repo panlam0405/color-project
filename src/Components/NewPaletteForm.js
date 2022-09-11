bfa197d001d25e5c82dabb54627f87cbf5d6aaf7
Panlam0405 <panlam0405@gmail.com>
2022-09-11 13:56:46 +0300

    Add ChromePicker and button to drawer

--- src/Components/NewPaletteForm.js
+++ src/Components/NewPaletteForm.js
@@ -5,20 +5,16 @@ import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import { ChromePicker } from "react-color";
+import { Button } from "@material-ui/core";
 
-const drawerWidth = 240;
+const drawerWidth = 350;
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -130,6 +126,22 @@ function NewPaletteForm() {
 					</IconButton>
 				</div>
 				<Divider />
+				<Typography variant='h4'>Design Your Palette</Typography>
+				<div>
+					<Button variant='contained' color='secondary'>
+						Clear Palette
+					</Button>
+					<Button variant='contained' color='primary'>
+						Random Color
+					</Button>
+				</div>
+				<ChromePicker
+					color='pink'
+					onChangeComplete={(newColor) => console.log(newColor)}
+				/>
+				<Button variant='contained' color='primary'>
+					Add Color
+				</Button>
 			</Drawer>
 			<main
 				className={clsx(classes.content, {
